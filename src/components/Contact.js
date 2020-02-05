af4e9ba292e1d7cde27e2dbb48273b71a31e457c
TylerOlli <tolli@nclcorp.com>
2020-02-05 09:56:11 -0500

    Minor Updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,12 +7,12 @@ function Contact(props) {
   return (
     <div>
       <h1>Contact</h1>
-      <ul className="Left-list">
-        <li><FontAwesomeIcon icon={faGithub} className="fontAwesomeList" /><a href="https://github.com/TylerOlli" className="item">Github</a></li>
-        <li><FontAwesomeIcon icon={faLinkedinIn} className="fontAwesomeList" /><a href="https://www.linkedin.com/in/tylerolli/" className="item">LinkedIn</a></li>
-        <li><FontAwesomeIcon icon={faPhone} className="fontAwesomeList" /><a href="tel:+9145844184" className="item">Phone</a></li>
-        <li><FontAwesomeIcon icon={faEnvelope} className="fontAwesomeList" /><a href="mailto:ollityler@gmail.com" className="item">Email</a></li>
-      </ul>
+      <div className="contactContainer">
+        <div className="contactItem"><FontAwesomeIcon icon={faGithub} className="fontAwesomeTop" /><a href="https://github.com/TylerOlli" className="item">Github</a></div>
+        <div className="contactItem"><FontAwesomeIcon icon={faLinkedinIn} className="fontAwesomeTop" /><a href="https://www.linkedin.com/in/tylerolli/" className="item">LinkedIn</a></div>
+        <div className="contactItem"><FontAwesomeIcon icon={faPhone} className="fontAwesomeTop" /><a href="tel:+9145844184" className="item">Phone</a></div>
+        <div className="contactItem"><FontAwesomeIcon icon={faEnvelope} className="fontAwesomeTop" /><a href="mailto:ollityler@gmail.com" className="item">Email</a></div>
+      </div>
     </div>
   )
 }
